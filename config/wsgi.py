1d16f635671ec9c72680b3e62346d5f83b761853
Sistemas GSOFT <gtovar@gsoftve.com>
2024-03-15 14:00:14 +0000

    [UPDATE] Configuracion de wsgi y utils

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
+import sys 
+
+# Agrega la ruta del proyecto Django al PYTHONPATH
+project_path = "/var/www/html/validator.movilpay.app/company_payments"
+sys.path.append(project_path)
+sys.path.append("/var/www/html/validator.movilpay.app/company_payments/config")
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_wsgi_application()
